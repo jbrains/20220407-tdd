cd2693966a2df6bde5d4249ccc9130a40bec26cf
J. B. Rainsberger <me@jbrains.ca>
2022-04-14 10:27:47 -0300

    We now gracefully handle the empty barcode.

--- app/src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
+++ app/src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
@@ -35,6 +35,16 @@ public class SellOneItemTest {
         Assertions.assertEquals("Product not found: 99999", display.getText());
     }
 
+    @Test
+    void emptyBarcode() {
+        final Display display = new Display();
+        final Sale sale = new Sale(display);
+
+        sale.onBarcode("");
+
+        Assertions.assertEquals("Scanning error: empty barcode", display.getText());
+    }
+
     public static class Sale {
         private final Display display;
 
@@ -43,7 +53,9 @@ public class SellOneItemTest {
         }
 
         public void onBarcode(String barcode) {
-            if ("12345".equals(barcode))
+            if ("".equals(barcode))
+                display.setText("Scanning error: empty barcode");
+            else if ("12345".equals(barcode))
                 display.setText("$7.95");
             else if ("23456".equals(barcode))
                 display.setText("$12.50");
