f31707ae25b952883b47dd09726149c977aedeb3
J. B. Rainsberger <me@jbrains.ca>
2022-04-14 09:55:23 -0300

    Made it easier to choose different prices for different barcodes.

--- app/src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
+++ app/src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
@@ -1,27 +1,52 @@
 package ca.jbrains.pos.test;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class SellOneItemTest {
     @Test
     void productFound() {
-        final Sale sale = new Sale();
         final Display display = new Display();
+        final Sale sale = new Sale(display);
 
         sale.onBarcode("12345");
 
         Assertions.assertEquals("$7.95", display.getText());
     }
 
+    @Disabled("wip: refactoring")
+    @Test
+    void anotherProductFound() {
+        final Display display = new Display();
+        final Sale sale = new Sale(display);
+
+        sale.onBarcode("23456");
+
+        Assertions.assertEquals("$12.50", display.getText());
+    }
+
     public static class Sale {
+        private final Display display;
+
+        public Sale(Display display) {
+            this.display = display;
+        }
+
         public void onBarcode(String barcode) {
+            display.setText("$7.95");
         }
     }
 
     public static class Display {
+        private String text;
+
         public String getText() {
-            return "$7.95";
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
         }
     }
 }
